2552e1868876d4aeb553292412aa1fccd6678232
eche <spwesn@gmail.com>
2018-10-16 14:00:17 -0700

    update files

--- js/main.js
+++ js/main.js
@@ -160,6 +160,7 @@ createRestaurantHTML = (restaurant) => {
 
 	const image = document.createElement('img');
 	image.className = 'restaurant-img';
+	image.alt = 'Image of ' + restaurant.name;
 	image.src = DBHelper.imageUrlForRestaurant(restaurant);
 	li.append(image);
 
@@ -178,7 +179,7 @@ createRestaurantHTML = (restaurant) => {
 	const more = document.createElement('a');
 	more.innerHTML = 'View Details';
 	more.href = DBHelper.urlForRestaurant(restaurant);
-	more.tabIndex = '3';
+	more.tabIndex = '0';
 	li.append(more);
 
 	return li;
